07f7b6b76976bb2b1db80453fe3671f4580eac64
Eric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2022-06-06 21:00:04 -0300

    Implementing Config Server with github and a simple service to consume

---
+++ greeting-service/src/main/java/com/sc/greetingservice/controller/GreetingController.java
@@ -0,0 +1,29 @@
+package com.sc.greetingservice.controller;
+
+import com.sc.greetingservice.configuration.GreetingConfig;
+import com.sc.greetingservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping(value = "/api")
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfig greetingConfig;
+
+    @GetMapping(value = "/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name) {
+        if (name.isEmpty()) name = greetingConfig.getDefaultValue();
+
+        return new Greeting(counter.incrementAndGet(), String.format(template, greetingConfig.getGreeting(), name));
+    }
+}
